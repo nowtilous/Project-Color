b6f259e2171b71a2bf8f6d6463f968c37eb32422
nowtilous <yoni24536@gmail.com>
2022-06-17 23:05:15 +0300

    color applying function & instant repaint

--- src/main/kotlin/com/github/nowtilous/intellijprojectcolor/ChooseColorAction.kt
+++ src/main/kotlin/com/github/nowtilous/intellijprojectcolor/ChooseColorAction.kt
@@ -2,27 +2,36 @@ package com.github.nowtilous.intellijprojectcolor
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataConstants
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.wm.WindowManager
 import javax.swing.JColorChooser
+import javax.swing.JFrame
+import javax.swing.SwingUtilities
 import javax.swing.UIManager
 import javax.swing.event.ChangeEvent
-import javax.swing.event.ChangeListener
 import javax.swing.plaf.ColorUIResource
 
 
 class ChooseColorAction : AnAction() {
 
+    private val mChooser = JColorChooser()
+    private lateinit var mActionEvent: AnActionEvent
     override fun actionPerformed(e: AnActionEvent) {
-        UIManager.put("TitlePane.background", ColorUIResource(0, 0, 0))
-
-        val chooser = JColorChooser()
-        chooser.selectionModel.addChangeListener { changeEvent ->
+        mActionEvent = e
+        mChooser.selectionModel.addChangeListener { changeEvent ->
             setTitleBarColor(changeEvent)
         }
-        val dialog = JColorChooser.createDialog(null, "Choose a title bar color", true, chooser, null, null)
+        val dialog = JColorChooser.createDialog(null, "Choose a title bar color", true, mChooser, null, null)
         dialog.isVisible = true
     }
     private fun setTitleBarColor(e: ChangeEvent?){
-        println(e)
+        UIManager.put("TitlePane.background", ColorUIResource(mChooser.color))
+
+        val project = mActionEvent.dataContext.getData(PlatformDataKeys.PROJECT)
+        SwingUtilities.updateComponentTreeUI(WindowManager.getInstance().getFrame(project))
+        println(mChooser.color)
     }
 }
 
