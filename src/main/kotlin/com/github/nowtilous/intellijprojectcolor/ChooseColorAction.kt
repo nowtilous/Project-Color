a7eac126ee0fd34c2f36a6ad1ad6eed80eeadd9c
nowtilous <yoni24536@gmail.com>
2022-06-18 11:47:01 +0300

    changed coloring method to color by component, which allows different coloring for different projects & added change lock

--- src/main/kotlin/com/github/nowtilous/intellijprojectcolor/ChooseColorAction.kt
+++ src/main/kotlin/com/github/nowtilous/intellijprojectcolor/ChooseColorAction.kt
@@ -1,11 +1,18 @@
 package com.github.nowtilous.intellijprojectcolor
 
+import com.esotericsoftware.kryo.NotNull
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataConstants
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.WindowManager
+import java.awt.Color
+import java.awt.Component
+import java.awt.Container
 import javax.swing.JColorChooser
 import javax.swing.JFrame
 import javax.swing.SwingUtilities
@@ -13,6 +20,8 @@ import javax.swing.UIManager
 import javax.swing.event.ChangeEvent
 import javax.swing.plaf.ColorUIResource
 
+val componentTrackedMap = mutableMapOf<Component, Boolean>()
+val currentProjectColor = mutableMapOf<Project, Color>()
 
 class ChooseColorAction : AnAction() {
 
@@ -20,18 +29,59 @@ class ChooseColorAction : AnAction() {
     private lateinit var mActionEvent: AnActionEvent
     override fun actionPerformed(e: AnActionEvent) {
         mActionEvent = e
-        mChooser.selectionModel.addChangeListener { changeEvent ->
-            setTitleBarColor(changeEvent)
+        mChooser.selectionModel.addChangeListener {
+            setTitleBarColor(mChooser.color, mActionEvent.dataContext.getData(PlatformDataKeys.PROJECT) as Project)
         }
         val dialog = JColorChooser.createDialog(null, "Choose a title bar color", true, mChooser, null, null)
         dialog.isVisible = true
     }
-    private fun setTitleBarColor(e: ChangeEvent?){
-        UIManager.put("TitlePane.background", ColorUIResource(mChooser.color))
 
-        val project = mActionEvent.dataContext.getData(PlatformDataKeys.PROJECT)
-        SwingUtilities.updateComponentTreeUI(WindowManager.getInstance().getFrame(project))
-        println(mChooser.color)
+}
+fun setTitleBarColor(color: Color, project: Project) {
+    val mainIdeComponent = (WindowManager.getInstance().getFrame(project) as JFrame).getComponent(0) as Container
+    var mainComponentPane: Container? = null
+    var titleBarComponent: Component? = null
+
+    for (component in mainIdeComponent.components){
+        if("JBLayeredPane" in component.toString()){
+            mainComponentPane = component as Container
+        }
     }
+    if (mainComponentPane == null) {
+        return
+    }
+
+    for(component in mainComponentPane.components){
+        if ("MenuFrameHeader" in component.toString()){
+            titleBarComponent = component
+        }
+    }
+
+    if (titleBarComponent == null) {
+        return
+    }
+
+
+    if(!componentTrackedMap.containsKey(titleBarComponent)){
+        componentTrackedMap[titleBarComponent] = false
+    }
+
+    if(componentTrackedMap[titleBarComponent] == false){
+        titleBarComponent.addPropertyChangeListener("background") {
+            if (it.newValue != (currentProjectColor[project] as Color).rgb) {
+                println("CHANGED")
+                titleBarComponent.background = currentProjectColor[project]
+            }
+        }
+
+        componentTrackedMap[titleBarComponent] = true
+    }
+
+    currentProjectColor[project] = color
+    titleBarComponent.background = color
+//    PropertiesComponent.getInstance(project).setValue("r", newColor.red, newColor.red)
+//    PropertiesComponent.getInstance(project).setValue("g", newColor.green, newColor.green)
+//    PropertiesComponent.getInstance(project).setValue("b", newColor.blue, newColor.blue)
+    println(color)
 }
 
--- src/main/kotlin/com/github/nowtilous/intellijprojectcolor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nowtilous/intellijprojectcolor/services/MyProjectService.kt
@@ -1,14 +1,34 @@
 package com.github.nowtilous.intellijprojectcolor.services
 
-import com.intellij.openapi.project.Project
+import com.github.nowtilous.intellijprojectcolor.ChooseColorAction
 import com.github.nowtilous.intellijprojectcolor.MyBundle
-import com.intellij.openapi.wm.WindowManager
+import com.github.nowtilous.intellijprojectcolor.setTitleBarColor
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.project.Project
+import java.awt.Color
 import javax.swing.UIManager
 
+//class MyProjectService(project: Project) : PersistentStateComponent<MyProjectService.State> {
 class MyProjectService(project: Project) {
 
+//    private var myState = State()
     init {
-        UIManager.put("TitlePane.background",javax.swing.plaf.ColorUIResource(250,250,250))
+//        if (myState.r != null && myState.g != null && myState.b!= null){
+//            setTitleBarColor(Color(myState.r!!, myState.g!!, myState.b!!), project)
+//        }
         println(MyBundle.message("projectService", project.name))
     }
+
+//    class State {
+//        var r: Int? = null
+//        var g: Int? = null
+//        var b: Int? = null
+//    }
+//    override fun getState(): State {
+//        return myState
+//    }
+//
+//    override fun loadState(state: State) {
+//        myState = state;
+//    }
 }
