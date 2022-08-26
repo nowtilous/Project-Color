0960705b98c94487fe5d56c7fb850750b4d700f4
nowtilous <yoni24536@gmail.com>
2022-08-26 11:41:03 +0300

    remove title bar searching logic from ProjectClosed

--- src/main/kotlin/com/github/nowtilous/projectcolor/listeners/ProjectManagerListener.kt
+++ src/main/kotlin/com/github/nowtilous/projectcolor/listeners/ProjectManagerListener.kt
@@ -4,6 +4,7 @@ import com.github.nowtilous.projectcolor.color_setters.ColorSetterFactory
 import com.github.nowtilous.projectcolor.gColorLockedComponentMap
 import com.github.nowtilous.projectcolor.gProjectColorLockedMap
 import com.github.nowtilous.projectcolor.gProjectColorMap
+import com.github.nowtilous.projectcolor.gProjectLockedComponentsMap
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
@@ -17,9 +18,15 @@ internal class ProjectManagerListener : ProjectManagerListener {
 
     override fun projectClosed(project: Project) {
         val currentColorSetter = ColorSetterFactory.getColorSetter()
-        gProjectColorMap.remove(project)
-        gProjectColorLockedMap.remove(project)
-        gColorLockedComponentMap.remove(currentColorSetter.findTitleBarComponent(project))
-        currentColorSetter.cleanUp(project)
+        try {
+            gProjectColorMap.remove(project)
+            gProjectColorLockedMap.remove(project)
+            for(component in gProjectLockedComponentsMap[project]!!){
+                gColorLockedComponentMap.remove(component)
+            }
+            gProjectLockedComponentsMap.remove(project)
+            currentColorSetter.cleanUp(project)
+        } catch (_: Exception) {
+        }
     }
 }
